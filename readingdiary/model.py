cd9fb6634bb40904864d18e58f14d9912e91d97c
lukasdpikesevg@gmail.com <lukasdpikesevg@gmail.com>
2026-03-09 17:34:12 -0500

    agregamos el metodo de instancia add_note para agregar notas al libro validando la pagina

--- readingdiary/model.py
+++ readingdiary/model.py
@@ -23,6 +23,14 @@ class Book:
         self.rating = Book.UNRATED
         self.notes = []
 
+    def add_note(self, text: str, page: int, date: datetime) -> bool:
+        if page > self.pages:
+            return False
+        note = Note(text, page, date)
+        self.notes.append(note)
+        return True
+
+
 
 
 
